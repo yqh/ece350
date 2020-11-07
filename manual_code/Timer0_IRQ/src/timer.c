7b793651475bb6fde6b2d6f48bf2d19aa3ebdf21
Yiqing Huang <yqihuang@gmail.com>
2020-11-07 15:42:41 -0500

    fixing comment typo

--- manual_code/Timer0_IRQ/src/timer.c
+++ manual_code/Timer0_IRQ/src/timer.c
@@ -110,7 +110,7 @@ __asm void TIMER0_IRQHandler(void)
  */
 void c_TIMER0_IRQHandler(void)
 {
-    /* ack inttrupt, see section  21.6.1 on pg 493 of LPC17XX_UM */
+    /* ack interrupt, see section  21.6.1 on pg 493 of LPC17XX_UM */
     LPC_TIM0->IR = BIT(0);  
     
     g_timer_count++ ;
