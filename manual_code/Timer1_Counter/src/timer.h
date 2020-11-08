b6ac75cd64064ffe5af06b6c592d717326314601
Yiqing Huang <yqihuang@gmail.com>
2020-11-07 19:44:06 -0500

    removed unused functions

--- manual_code/Timer1_Counter/src/timer.h
+++ manual_code/Timer1_Counter/src/timer.h
@@ -1,17 +1,11 @@
 /**
  * @brief timer.h - Timer header file
  * @author Y. Huang
- * @date 2013/02/12
+ * @date 2020/11/01
  */
 #ifndef _TIMER_H_
 #define _TIMER_H_
 #include <LPC17xx.h>
 
-extern int reset_counter(uint8_t n_timer);
-extern int reset_start_counter(uint8_t ntimer);
-extern int start_counter(uint8_t n_timer);
-extern int stop_counter(uint8_t n_timer);
-
-extern uint32_t timer_init ( uint8_t n_timer );  /* initialize timer n_timer PCLK */
 extern uint32_t timer_init_100MHZ(uint8_t n_timer);
 #endif /* ! _TIMER_H_ */
