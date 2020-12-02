80da4a7e9cb753ad801ec899b14573c5ee4f6142
unknown <yqihuang@gmail.com>
2020-12-02 16:21:10 -0500

    lab5 auto tests

---
+++ manual_code/auto_test/ae_lab5/AE-Lib/src/common_ext.h
@@ -0,0 +1,10 @@
+/**
+ * @file  common_ext.h
+ * @brief Any self-defined macros or data structures that you want to share
+ *        between the kernel and the user spaces are here
+ * @see   common.h
+ */
+ 
+ /* The following is a self-defined macro */
+ #define MY_DUMMY_MACRO1 300
+
