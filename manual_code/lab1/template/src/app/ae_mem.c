d6d5a4c22073644219c863dc3c1d956e1c7f13aa
s55ye <sophia.ye@uwaterloo.ca>
2021-01-26 22:02:41 -0500

    Test 2 and 3 implementation

--- manual_code/lab1/template/src/app/ae_mem.c
+++ manual_code/lab1/template/src/app/ae_mem.c
@@ -58,8 +58,92 @@ int test_coales(void) {
 	return result == 31;
 }
 
+int test_reuse_freed(void) {
+	void *p[4];
+	U32 result = 0;
+	if (countNodes()==1){
+		result |= BIT(0);
+	}
+
+	p[0] = mem_alloc(12);
+	p[1] = mem_alloc(24);
+	p[2] = mem_alloc(48);
+
+    if (countNodes()==4){
+    	result |= BIT(1);
+    }
+
+    mem_dealloc(p[0]);
+    if (countNodes()==4){
+		result |= BIT(2);
+	}
+
+    p[3] = mem_alloc(12);
+	if (countNodes()==4){
+		result |= BIT(3);
+	}
+
+	mem_dealloc(p[1]);
+	mem_dealloc(p[2]);
+	mem_dealloc(p[3]);
+	if (countNodes()==1){
+		result |= BIT(4);
+	}
+
+	if(memLeakCheck()==1){
+		result |= BIT(5);
+	}
+
+	return result == 63;
+}
+
+int test_3(void) {
+	void *p[4];
+	U32 result = 0;
+	if (countNodes()==1){
+		result |= BIT(0);
+	}
+
+	p[0] = mem_alloc(12);
+	p[1] = mem_alloc(24);
+	p[2] = mem_alloc(48);
+
+    if (countNodes()==4){
+    	result |= BIT(1);
+    }
+
+	mem_dealloc(p[1]);
+    if (countNodes()==4){
+		result |= BIT(2);
+	}
+
+	p[3] = mem_alloc(12);
+	if (countNodes()==5){
+		result |= BIT(3);
+	}
+
+	mem_dealloc(p[3]);
+    if (countNodes()==4){
+		result |= BIT(4);
+	}
+
+	mem_dealloc(p[0]);
+	mem_dealloc(p[2]);
+	if (countNodes()==1){
+		result |= BIT(5);
+	}
+
+	if(memLeakCheck()==1){
+		result |= BIT(6);
+	}
+
+	return result == 127;
+}
+
 int test_mem(void) {
 	printf("test_coales passed: %x\r\n", test_coales());
+	printf("test_reuse_freed passed: %x\r\n", test_reuse_freed());
+	printf("test_3 passed: %x\r\n", test_3());
 
     void *p[4];
     int n;
