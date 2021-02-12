3e6cc0c621ee443b9cdc2a28d37a86198b77a453
Raymond Yang <stingray@Raymonds-MacBook-Pro.local>
2021-02-11 23:07:47 -0800

    fixed p stack

--- manual_code/lab2/TM/src/kernel/k_mem.c
+++ manual_code/lab2/TM/src/kernel/k_mem.c
@@ -92,9 +92,11 @@ U32* k_alloc_k_stack(task_t tid)
     return g_k_stacks[tid+1];
 }
 
-U32* k_alloc_p_stack(task_t tid)
-{
-    return mem_alloc(g_tcbs[tid].u_stack_size);
+U32* k_alloc_p_stack(task_t tid) {
+    void* mem = k_mem_alloc(g_tcbs[tid].u_stack_size);
+    Node* node = (Node*)mem - 1;
+    node->owner = 0;
+    return (U32*) ( mem + g_tcbs[tid].u_stack_size);
 }
 
 int k_mem_init(void) {
